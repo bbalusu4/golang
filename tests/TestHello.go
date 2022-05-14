09f7f4aafbdf00fa345e8f0a4e875232eeeba8bc
Bala Balusu <bbala031@gmail.com>
2022-05-14 10:54:20 -0400

    main fucntion changed and updated test class

--- tests/TestHello.go
+++ tests/TestHello.go
@@ -4,8 +4,9 @@ import "testing"
 //testing Hello World program
 
 func TestHello(t *testing.T){
-  testFunc := main() //main function does not return any value
+ // testFunc := main() //main function does not return any value
   testString := "Hello World"
+  testFunc := Hello() //new function created to have retrun value and called to print from main func
 
   if testFunc != testString {
 	  t.errorf("testFunc %q testString %q", testFunc, testString)
